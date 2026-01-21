961060d4422b80042858340748ac7067f1245226
badger <jonbraunsma@gmail.com>
2026-01-21 02:31:45 +0000

    Find it odd how tests worked using this, but docfx doesn't work... but ok

--- PDFLib.Chromium.Hosting/PdfServiceCollectionextensions.cs
+++ PDFLib.Chromium.Hosting/PdfServiceCollectionextensions.cs
@@ -8,41 +8,40 @@ namespace PDFLib.Chromium.Hosting;
 /// </summary>
 public static class PdfServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds the <see cref="ChromiumBrowser"/> as a singleton service and its hosted service for lifecycle management.
+    /// </summary>
     /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
-    extension(IServiceCollection services)
+    /// <param name="configuration">The configuration section to use for <see cref="BrowserOptions"/>.</param>
+    /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+    public static IServiceCollection AddPdfService(this IServiceCollection services, IConfiguration configuration)
     {
-        /// <summary>
-        /// Adds the <see cref="ChromiumBrowser"/> as a singleton service and its hosted service for lifecycle management.
-        /// </summary>
-        /// <param name="configuration">The configuration section to use for <see cref="BrowserOptions"/>.</param>
-        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-        public IServiceCollection AddPdfService(IConfiguration configuration)
-        {
-            services.Configure<BrowserOptions>(configuration);
-            return services.AddPdfService();
-        }
+        services.Configure<BrowserOptions>(configuration);
+        return services.AddPdfService();
+    }
 
-        /// <summary>
-        /// Adds the <see cref="ChromiumBrowser"/> as a singleton service and its hosted service for lifecycle management.
-        /// </summary>
-        /// <param name="setupAction">An action to configure the <see cref="BrowserOptions"/>.</param>
-        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-        public IServiceCollection AddPdfService(Action<BrowserOptions> setupAction)
-        {
-            services.Configure(setupAction);
-            return services.AddPdfService();
-        }
+    /// <summary>
+    /// Adds the <see cref="ChromiumBrowser"/> as a singleton service and its hosted service for lifecycle management.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+    /// <param name="setupAction">An action to configure the <see cref="BrowserOptions"/>.</param>
+    /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+    public static IServiceCollection AddPdfService(this IServiceCollection services, Action<BrowserOptions> setupAction)
+    {
+        services.Configure(setupAction);
+        return services.AddPdfService();
+    }
 
-        /// <summary>
-        /// Adds the <see cref="ChromiumBrowser"/> as a singleton service and its hosted service for lifecycle management.
-        /// </summary>
-        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-        public IServiceCollection AddPdfService()
-        {
-            services.AddSingleton<ChromiumBrowser>(sp => ChromiumBrowser.Instance);
-            services.AddSingleton<PdfService>();
-            services.AddHostedService<PdfService>(sp => sp.GetRequiredService<PdfService>());
-            return services;
-        }
+    /// <summary>
+    /// Adds the <see cref="ChromiumBrowser"/> as a singleton service and its hosted service for lifecycle management.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+    /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+    public static IServiceCollection AddPdfService(this IServiceCollection services)
+    {
+        services.AddSingleton<ChromiumBrowser>(sp => ChromiumBrowser.Instance);
+        services.AddSingleton<PdfService>();
+        services.AddHostedService<PdfService>(sp => sp.GetRequiredService<PdfService>());
+        return services;
     }
 }
